bb6e9dbe02c0d8d01df95f21e9798e02ceab3a1f
DAH746 <ahdanyal@googlemail.com>
2021-11-01 15:38:47 +0000

    First commit

--- MegaFile.tf
+++ MegaFile.tf
@@ -4,7 +4,51 @@ provider "aws" {
 
 resource "aws_vpc" "prod-vpc" {
  cidr_block = "10.0.0.0/16"
+} //todo subnets
+
+resource "aws_subnet" "publicSubnet" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "publicSubnet"
+  }
 }
+// START - Private subnets with respective AZs
+resource "aws_subnet" "privateSubnetAZ2" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "privateSubnet"
+  }
+
+  availability_zone_id = "euw2-az2"
+}
+
+resource "aws_subnet" "privateSubnetAZ3" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "privateSubnet"
+  }
+
+  availability_zone_id = "euw2-az3"
+}
+
+resource "aws_subnet" "privateSubnetAZ1" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "privateSubnet"
+  }
+
+  availability_zone_id = "euw2-az1"
+}
+
+//END - Private subnets with respective AZs
 
 resource "aws_s3_bucket" "log-conts-ALB" {
  bucket = "Log-bucket-for-conts-ALB-3674"
@@ -28,8 +72,17 @@ resource "aws_internet_gateway" "gw" {
         vpc_id = aws_vpc.prod-vpc.id
 }
 
+// WAF - FIREWALL
+resource "aws_wafregional_geo_match_set" "WAF-regional-UK" {
+  name = "geo_match_set"
 
-resource "aws_security_group" "allow_http/s" {
+  geo_match_constraint {
+    type  = "Country"
+    value = "UK"
+  }
+}
+
+resource "aws_security_group" "allow_https" {
   name        = "allow_http/s"
   description = "Allow HTTP/S inbound traffic"
   vpc_id      = aws_vpc.prod-vpc.id
@@ -41,32 +94,26 @@ resource "aws_security_group" "allow_http/s" {
       to_port          = 443
       protocol         = "tcp"
       cidr_blocks      = aws_vpc.prod-vpc.cidr_block
-    }
+    },
     {
       description      = "HTTP from VPC"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = aws_vpc.prod-vpc.cidr_block
-  ]
-   }
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = [":
-  ]
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
     }
+  ]
+  egress = [
+      {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [
+          "0.0.0.0/0"]
+        ipv6_cidr_blocks = [
+          ":"]
+      }
   ]
 
   tags = {
@@ -78,11 +125,11 @@ resource "aws_lb" "uk-alb" {
   name               = "uk-alb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    =
-  subnets            =
+  security_groups    = [aws_security_group.allow_https.id]
+  subnets            = aws_subnet.publicSubnet.*.id
 
   access_logs {
-    bucket  = log-conts-ALB.bucket
+    bucket  = aws_s3_bucket.log-conts-ALB.bucket
     prefix  = "uk-alb-tf"
     enabled = true
   }
@@ -90,7 +137,49 @@ resource "aws_lb" "uk-alb" {
   tags = {
     Environment = "production"
   }
+}
 
+// ECS
 
+resource "aws_ecs_cluster" "ECS_cluster" {
+  name = "tf-ecs-cluster"
+}
 
+resource "aws_ecs_task_definition" "task_definition" {
+  family                = "worker"
+  container_definitions = jsonencode(
+    [
+      {
+        essential: true,
+        memory: 512,
+        name: "worker",
+        cpu: 2,
+        image: "${REPOSITORY_URL}:latest",
+        environment: []
+      }
+    ])
+}
 
+resource "aws_ecs_service" "main" {
+  name            = "staging"
+  cluster         = aws_ecs_cluster.ECS_cluster.id
+  task_definition = aws_ecs_task_definition.task_definition
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+//  load_balancer {
+//    target_group_arn = aws_lb_target_group.staging.arn
+//    container_name   = "sproutlyapi"
+//    container_port   = 4000
+//  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [euw2-az1, euw2-az2, euw2-az3]"
+  }
+
+  tags = {
+    Environment = "staging"
+    Application = "webApp"
+  }
+}
