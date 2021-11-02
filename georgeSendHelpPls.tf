e46f80f9383dad86807248aedf4e430c7be0f2c6
MonChiSub <41806105+MonChiSub@users.noreply.github.com>
2021-11-02 09:55:48 +0000

    Update georgeSendHelpPls.tf
    removed useless junk yo

--- georgeSendHelpPls.tf
+++ georgeSendHelpPls.tf
@@ -1,45 +1,4 @@
-# THIS 
-resource "aws_s3_bucket" "log-conts-ALB" {
-    bucket = "Log-bucket-for-conts-ALB-3674"
-    acl = "private"
-
-    versioning {
-        enabled = true
-    }
-
-    lifecycle_rule {
-        id = "monthlyRotation"
-        prefix = "logs/"
-        enabled = true
-
-        expiration {
-            days = 30
-        }
-    }
-}
-
-resource "aws_s3_bucket" "glacierLogs" {
-    bucket = "glacierLogs"
-    acl = "private"
-    lifecycle_rule {
-        id = "glacierLogsIDYo"
-        enabled = true
-        prefix = "logs/"
-
-        transition {
-            days = 1
-            storage_class = "GLACIER"
-        }
-
-        expiration {
-            days = 365
-        }
-    }
-}
-
-# Send help pls aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-# OR
-# THIS
+#we think this one xd
 resource "aws_s3_bucket" "log-conts-ALB" {
     bucket = "Log-bucket-for-conts-ALB-3674"
     acl = "private"
